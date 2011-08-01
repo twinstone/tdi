d3b267621e9e45b9a9c6ddb02cec92c980aba2bf
stepan <stepan.mik@etnetera.cz>
2011-08-01 13:26:13 +0200

    Prototype of selenium JUnit test.

--- test/org/twinstone/tdi/TestDriver.java
+++ test/org/twinstone/tdi/TestDriver.java
@@ -2,20 +2,36 @@ package org.twinstone.tdi;
 
 import java.io.File;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestDriver {
 	WebDriver driver;
 	
+	@BeforeClass
+	public static void setProp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\stepan\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+	}
+	
 	@Before
 	public void initDriver() {
-		driver = new HtmlUnitDriver();
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability("chrome.binary", "C:\\Users\\stepan\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe");
+		driver = new ChromeDriver(capabilities);
+	}
+	
+	@After
+	public void quitDriver() {
+		driver.quit();
 	}
 	
 	@Test
