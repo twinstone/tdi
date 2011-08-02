b591a8eba707236d1adf07bd2aafd7be312784cb
centi <ivan.centes@etnetera.cz>
2011-08-02 09:32:13 +0200

    Test updates

--- test/org/twinstone/tdi/BrowserPages.java
+++ test/org/twinstone/tdi/BrowserPages.java
@@ -1,7 +1,5 @@
 package org.twinstone.tdi;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.File;
 
 import org.junit.After;
@@ -10,6 +8,10 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.base.Function;
+
 import static org.junit.Assert.*;
 
 public class BrowserPages {
@@ -17,6 +19,16 @@ public class BrowserPages {
 	protected WebDriver driver;
 	
 	protected static final File base = new File("tests").getAbsoluteFile();
+	protected static final String[] pages = {
+		new File(base,"tdi-ajax.html").toURI().toString(),
+		new File(base,"tdi-tools.html").toURI().toString(),
+		new File(base,"tdi-ajax-ui.html").toURI().toString(),
+		new File(base,"tdi-ajax-request.html").toURI().toString(),
+		new File(base,"tdi-ajax-response-error.html").toURI().toString(),
+		new File(base,"tdi-ajax-response-events-prevent.html").toURI().toString(),
+		new File(base,"tdi-ajax-response-events.html").toURI().toString(),
+		new File(base,"tdi-ajax-response-responses.html").toURI().toString()
+	};
 	
 	@BeforeClass
 	public static void assertBase() {
@@ -25,9 +37,23 @@ public class BrowserPages {
 	
 	@Test
 	public void index() {
-		driver.get(new File(base,"index.html").toURI().toString());
-		WebElement element = driver.findElement(By.tagName("h1"));
-		assertEquals("Title",element.getText());
+		for (String pageURI : pages) {
+			driver.get(pageURI);
+			
+			WebDriverWait waitForResult = new WebDriverWait(driver, 60);
+	        WebElement resultElm = waitForResult.until(new Function<WebDriver, WebElement>() {
+	            @Override
+	            public WebElement apply(WebDriver driver) {
+	                WebElement elm = driver.findElement(By.id("qunit-testresult"));
+	                if (elm != null && !elm.getText().contains("Running")) {
+	                    return elm;
+	                }
+	                return null;
+	            }
+	        });
+	        
+	        assertTrue(pageURI, resultElm.getText().contains("0 failed"));
+		}
 	}
 	
 	@After
--- test/org/twinstone/tdi/NeutralTest.java
+++ test/org/twinstone/tdi/NeutralTest.java
@@ -3,12 +3,15 @@ package org.twinstone.tdi;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class NeutralTest extends BrowserPages {
 	
 	@Before
 	public void initDriver() {
-		driver = new HtmlUnitDriver(true);
+		DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit();
+		capabilities.setCapability("ThrowExceptionOnScriptError", false);
+		driver = new HtmlUnitDriver(capabilities);
 	}
 	
 	@After
