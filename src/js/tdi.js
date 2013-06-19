a955d1dd56746b139df66fed4dcdaf32f78289f4
Ivan Čentéš <ivan.centes@etnetera.cz>
2013-06-19 14:43:02 +0200

    Removed version checker for jQuery

--- src/js/tdi.js
+++ src/js/tdi.js
@@ -5,41 +5,12 @@ if ( typeof jQuery == 'undefined' ) {
 /**
  * <p>Javascript library which enables the communication between the UI and the application, using
  * the <a href="http://wbase.etn/doku.php?id=ice:eris:infusion_js:ajax:protokol">Infusion AJAX protocol</a>.</p>
+ * Minimum jQuery version required for TDI is 1.7.0.
  * @module TDI
  */
 var TDI = function($) {
 
-	/**
-	 * <p>Minimum jQuery version required for TDI.</p>
-	 * @type {String}
-	 * @property _jQueryMinVersion
-	 * @private
-	 * @final
-	 */
-	var _jQueryMinVersion = '1.7.0';
-
-	/**
-	 * <p>Checks whether the minimum requirements are satisfied.</p>
-	 * @method _checkDependencies
-	 * @private
-	 */
-	function _checkDependencies() {
-		var versions = {
-			min : _jQueryMinVersion.replace( /\./g, '' ),
-			orig : jQuery.fn.jquery.replace( /\./g, '' )
-		}, i;
-		for ( i = 0, l = Math.max( versions.min.length, versions.orig.length ); i < l; i++ ) {
-			versions.min += ( versions.min.charAt(i) )?'':'0';
-			versions.orig += ( versions.orig.charAt(i) )?'':'0';
-		}
-
-		if ( versions.orig < versions.min ) {
-			throw( 'TDI requires jQuery version '+_jQueryMinVersion+' or higher!' );
-		}
-	}
-
-	// initialization
-		_checkDependencies();
+	// PRIVATE STUFF -----------------------------------------------------------
 
 	// PUBLIC STUFF ------------------------------------------------------------
 		return {};
