7140e780b7e2e3198a7e64480b5e5a02476be8d3
Ivan Centes <ivan.centes@etnetera.cz>
2014-09-04 22:43:07 +0200

    Doc

---
+++ doc/api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TDI.Ajax",
+        "TDI.Ajax.Request",
+        "TDI.Ajax.Response",
+        "TDI.Tools"
+    ],
+    "modules": [
+        "TDI"
+    ],
+    "allModules": [
+        {
+            "displayName": "TDI",
+            "name": "TDI",
+            "description": "<p>Javascript library which enables the communication between the UI and the application, using\nthe <a href=\"http://wbase.etn/doku.php?id=ice:eris:infusion_js:ajax:protokol\">Infusion AJAX protocol</a>.</p>\nMinimum jQuery version required for TDI is 1.7.0."
+        }
+    ]
+} };
+});
